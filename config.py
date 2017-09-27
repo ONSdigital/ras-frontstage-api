01e5e1523ed523646bdd58c3dbf3188f55484cba
Andrew Millar <andrew.millar@qa.com>
2017-09-27 14:37:16 +0100

    adding logging configuration

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
+import os
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
     PORT = 8082
+    LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO')
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'DEBUG')
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
+    LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'DEBUG')
--- frontstage_api/__init__.py
+++ frontstage_api/__init__.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 
+from frontstage_api.logger_config import logger_initial_config
+
 
 app = Flask(__name__)
 
 app_config = 'config.{}'.format(os.environ.get('APP_SETTINGS', 'Config'))
 app.config.from_object(app_config)
+
+logger_initial_config(service_name='ras-frontstage-api', log_level=app.config['LOGGING_LEVEL'])
 
 
 import frontstage_api.views.secure_messaging  # NOQA

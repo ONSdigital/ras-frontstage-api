fbfcd389c5e6bf919814fe738c3d30bd01fab842
tej99 <tejas.patel@qa.com>
2017-11-21 10:49:00 +0000

    Remove default git info file (#22)
    * Remove default git_info
    and update info tests
    
    * Delete git_info file
    
    * Delete git_info file
    
    * Add git_info to gitignore
    
    * Use standard json instead of flask json

--- tests/Resources/test_info.py
+++ tests/Resources/test_info.py
@@ -1,4 +1,7 @@
+import json
+import os
 import unittest
+from pathlib import Path
 
 from frontstage_api import app
 
@@ -9,14 +12,21 @@ class TestInfo(unittest.TestCase):
         self.app = app.test_client()
 
     def test_info_no_git_info(self):
+        if Path('git_info').exists():
+            os.remove(Path('git_info').absolute())
+
         response = self.app.get("/info")
 
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('"name": "ras-frontstage-api"'.encode() in response.data)
+        self.assertIn('"name": "ras-frontstage-api"'.encode(), response.data)
+        self.assertNotIn('"test": "test"'.encode(), response.data)
 
     def test_info_with_git_info(self):
+        with open('git_info', 'w') as outfile:
+            json.dump({"test": "test"}, outfile)
+
         response = self.app.get("/info")
 
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('"name": "ras-frontstage-api"'.encode() in response.data)
-        self.assertTrue('"test": "test"'.encode() in response.data)
+        self.assertIn('"name": "ras-frontstage-api"'.encode(), response.data)
+        self.assertIn('"test": "test"'.encode(), response.data)
